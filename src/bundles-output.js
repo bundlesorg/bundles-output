e15c235698f30c6e427b6fb4ce349ed246039879
Zimmee <thezimmee@gmail.com>
2019-05-09 14:41:01 -0600

    feature(removed): Iterate through `bundles.removed` and remove any previously created files.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   package.json
    #	modified:   src/bundles-output.js
    #
    # Untracked files:
    #	TODO.md
    #
    
    [BRANCH: master]

--- src/bundles-output.js
+++ src/bundles-output.js
@@ -3,35 +3,47 @@
 import fs from 'fs-extra'
 import path from 'path'
 
-module.exports = (bundle = {}, bundler = {}) => {
+export default (bundle = {}, bundler = {}) => {
   const cwd = process.cwd()
   // Set default options.
-  bundler.options = Object.assign({
-    to: cwd,
+  const options = bundler.options = Object.assign({
+    to: getOutputPath,
+    dir: '',
     root: cwd,
     incremental: !!bundle.changed,
     fs: {
       encoding: 'utf8',
       flag: 'w'
     }
-  }, bundler.options)
+  }, bundler.options || {})
   // Create a promises Array to later return.
   const promises = []
+  // Ensure options.to is a function.
+  if (typeof options.to === 'string') options.dir = options.to
+  if (typeof options.to !== 'function') options.to = getOutputPath
   // Iterate through each output file.
-  bundle[bundler.options.incremental ? 'changed' : 'output'].forEach((file, i) => {
+  bundle[options.incremental ? 'changed' : 'output'].forEach((file, i) => {
     // Determine the output path.
-    let outputPath = typeof bundler.options.to === 'function'
-      ? bundler.options.to(file, bundler, bundle)
-      : path.resolve(bundler.options.to, bundler.options.root
-        ? path.relative(bundler.options.root, file.source.path)
-        : file.source.path)
+    let outputPath = options.to(file, bundler.options)
     // If outputPath is a string, add promise to output file.
     if (typeof outputPath === 'string') {
-      const fsOptions = Object.assign({}, bundler.options.fs)
+      const fsOptions = Object.assign({}, options.fs)
+      file.to = outputPath
       if (file.isBuffer) fsOptions.encoding = null
       promises.push(fs.outputFile(outputPath, file.content, fsOptions))
     }
+  });
+  // Delete removed files from their previously output path.
+  (bundle.removed || []).forEach((file, i) => {
+    let outputPath = file.to || options.to(file, bundler)
+    if (typeof outputPath === 'string') promises.push(fs.remove(outputPath))
   })
   // Once all promises resolve, return the bundle.
   return Promise.all(promises).then((results) => bundle)
+}
+
+function getOutputPath (file, options = {}) {
+  return path.resolve(options.dir, options.root
+    ? path.relative(options.root, file.source.path)
+    : file.source.path)
 }
